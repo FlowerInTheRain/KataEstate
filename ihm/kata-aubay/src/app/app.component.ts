ee3213eb47a7bf0c06c6008a504800cb64c3f2c0
root <root@SidHome.>
2025-05-11 12:11:04 +0200

    fix conflict

--- ihm/kata-aubay/src/app/app.component.ts
+++ ihm/kata-aubay/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {CommonModule} from '@angular/common';
-import {PropertiesComponent} from './properties/properties.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, PropertiesComponent],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.scss'
